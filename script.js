85e50d66e6776a55e3c0b980cbb71ef217560252
amineakkour <amineakour6@gmail.com>
2024-03-08 20:28:25 +0100

    update files

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const widthRange = document.querySelector("#width-input")
 const heightRange = document.querySelector("#height-input")
+const createblocksButton = document.querySelector("#create")
+const clearBlocksButton = document.querySelector("#clear")
 
 function startGame(){
     const startBtn = document.querySelector("#start")
@@ -14,7 +16,7 @@ function startGame(){
             board.className = ""
             startBtn.remove()
             createGrid(parseInt(widthRange.value), parseInt(heightRange.value))
-        }, 500); 
+        }, 600); 
     }
 
 }
@@ -25,23 +27,20 @@ function createGrid(width, height){
     const blocks = document.querySelectorAll(".block")
     const colorInput = document.querySelector("#color")
 
-    console.log(height, width)
-
-
     if(blocks){
         blocks.forEach(block => {
             block.remove()
         });
     }
     
-    console.log(colorInput.value)
-    
     for(i = 0; i < width * height; i++){
         const block = document.createElement("div")
         block.className = "block"
         
-        block.addEventListener("dragenter", () =>{
-            block.style.backgroundColor = colorInput.value
+        block.addEventListener("dragenter", e => {
+                e.preventDefault()
+    
+                block.style.backgroundColor = colorInput.value;
         })
 
         blocksWrapper.appendChild(block)
@@ -55,12 +54,16 @@ widthRange.addEventListener("change", () => {
     const widthScreen = document.querySelector(".width span")
 
     widthScreen.textContent = widthRange.value
-    createGrid(parseInt(widthRange.value), parseInt(heightRange.value))
+    // createGrid(parseInt(widthRange.value), parseInt(heightRange.value))
 })
 
 heightRange.addEventListener("change", () => {
     const heightScreen = document.querySelector(".height span")
 
     heightScreen.textContent = heightRange.value
+    // createGrid(parseInt(widthRange.value), parseInt(heightRange.value))
+})
+
+createblocksButton.addEventListener("click", () => {
     createGrid(parseInt(widthRange.value), parseInt(heightRange.value))
 })
